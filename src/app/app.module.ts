a7f99179a2df94929486dfe7587376955853cd03
srirammohan562 <srirammohan562@gmail.com>
2023-06-15 10:21:42 +0530

    Init Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './auth/login/login.component';
+import { LogoutComponent } from './auth/logout/logout.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { AboutusComponent } from './home/aboutus/aboutus.component';
+import { ContactusComponent } from './home/contactus/contactus.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    LogoutComponent,
+    RegisterComponent,
+    AboutusComponent,
+    ContactusComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
